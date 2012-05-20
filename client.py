f3a9af48e24baa4cf0c8e4f53d1125cdb50cbd39
Marcos Lopez <dev@scidentify.info>
2012-05-20 19:38:13 -0400

    fix tab indenting and exception class

--- client.py
+++ client.py
@@ -11,15 +11,15 @@ import time
 # set the db class
 d = db()
 
-"""check if its alreayd running 
+"""check if its already running 
 you can comment this out if run time of this script is less
 than the frequency of time to run this script at
 (exectime < run-frequency)
 """
 for i in os.popen('ps aux | grep client.py').readlines():
-	if 'python client.py' in i and not str(os.getpid()) in i:
-		print 'Currently running in the background, waiting...'
-		sys.exit()
+  if 'python client.py' in i and not str(os.getpid()) in i:
+    print 'Currently running in the background, waiting...'
+    sys.exit()
 
 
 
@@ -34,17 +34,15 @@ results = d.session.query(UserProfile).filter_by(status='active')
 
 
 for i in results:
-	print i.user_id
-	# get single object by its pk
-	results_get = d.session.query(UserProfile).get(i.id)
-	
-	# update an entry - will set status to inactive
-	update_data = {'status': 'inactive'}
-	d.session.query(UserProfile).filter_by(id=id).update(update_data)
-	# commit the changes!
-	d.session.commit()
+  print i.user_id
+  # get single object by its pk
+  results_get = d.session.query(UserProfile).get(i.id)
 
-	print 'Inactivated user id: %s' % (i.user_id)
-	
-	
-	
+  # update an entry - will set status to inactive
+  update_data = {'status': 'inactive'}
+  d.session.query(UserProfile).filter_by(id=id).update(update_data)
+  # commit the changes!
+  d.session.commit()
+
+  print 'Inactivated user id: %s' % (i.user_id)
+
--- dbaccess.py
+++ dbaccess.py
@@ -1,62 +1,74 @@
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import mapper, sessionmaker
 
+# Authored Marcos Lopez / dev@scidentify.info
+
 # table class mappings
 class SampleTable(object):
-	pass
+  pass
 
 class UserProfile(object):
-	pass
+  pass
+
+# custom exceptions
+class NoConnection(BaseException):
+  pass
 
 # database class 
 class db(object):
-	engine = None
-	metadata = None
+  """ Database class params:
+    dbuser: database user
+    dbpass: duhh..
+    dbname: database name
+    dbhost: host ip/name of db
+    localhost: set this to override dbhost value to localhost
+   """
+  engine = None
+  metadata = None
+  dbuser = None
+  dbpass = None
+  dbname = None
+  dbhost = None
 
-	dbuser = None
-	dbpass = None
-	dbname = None
-	dbhost = None
+  def __init__(self, dbuser=None, dbpass=None, dbname=None, dbhost=None, localhost=False):
+    """ initialize connection parameters if passed """
+    if dbuser:
+      self.dbuser = dbuser
+    if dbpass:
+      self.dbpass = dbpass
+    if dbname:
+      self.dbname = dbname
+    if dbhost:
+      self.dbhost = dbhost
 
-	def __init__(self, dbuser=None, dbpass=None, dbname=None, dbhost=None, localhost=False):
-		""" initialize connection parameters if passed """
-		if dbuser:
-			self.dbuser = dbuser
-		if dbpass:
-			self.dbpass = dbpass
-		if dbname:
-			self.dbname = dbname
-		if dbhost:
-			self.dbhost = dbhost
+    if localhost:
+      self.dbhost = 'localhost'
 
-		if localhost:
-			self.dbhost = 'localhost'
-		
-		if not self.connect():
-			print 'Database connection error'
-			exit
+    if not self.connect():
+      raise NoConnection("Database connection error")
+      exit
 
-		# map the tables
-		sampletable = Table('sampletable', self.metadata, autoload=True)
-		userprofile = Table('userprofile', self.metadata, autoload=True)
-		mapper(SampleTable, sampletable)
-		mapper(UserProfile, userprofile)
+    # map the tables
+    sampletable = Table('sampletable', self.metadata, autoload=True)
+    userprofile = Table('userprofile', self.metadata, autoload=True)
+    mapper(SampleTable, sampletable)
+    mapper(UserProfile, userprofile)
 
-		session = sessionmaker(bind=self.engine)
-		self.session = session()
+    session = sessionmaker(bind=self.engine)
+    self.session = session()
 
-	def connect(self):
-		self.engine = create_engine('mysql://%s:%s@%s/%s' % (
-			self.dbuser, self.dbpass, self.dbhost, self.dbname
-		), echo=False)
-		self.metadata = MetaData(self.engine)
-		return self.metadata
+  def connect(self):
+    """ connects to the engine, returns database metadata """
+    self.engine = create_engine('mysql://%s:%s@%s/%s' % (
+      self.dbuser, self.dbpass, self.dbhost, self.dbname
+    ), echo=False)
+    self.metadata = MetaData(self.engine)
+    return self.metadata
 
-	def disconnect(self):
-		self.engine.disconnect()
-		
-	
-		
+  def disconnect(self):
+    self.engine.disconnect()
+
 if __name__ == '__main__':
-	# c = db()
-	
+  # running from command line
+  # c = db()
+  
