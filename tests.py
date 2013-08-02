28c9dbe45bb55ff2642111bdfb9f68709b2ce5a3
Mike Lopez <dev@scidentify.info>
2013-08-02 03:25:27 -0400

    settings & test file

---
+++ tests.py
@@ -0,0 +1,47 @@
+import sys
+from termprint import termprint
+from unittest import TestCase, TestSuite, TextTestRunner
+
+import simplejson
+
+
+class AssertionError:
+    # simple assertion error output
+    def __init__(self, msg, exit=False):
+        termprint("ERROR", msg)
+        if exit:
+            sys.exit(1)
+
+class TestDB(TestCase):
+    """ Base test class for ami functionality that will be used """
+
+    PBX = getattr(settings, "PBX")
+    AMI_USER = getattr(settings, "AMI_USER")
+    AMI_PASS = getattr(settings, "AMI_PASS")
+
+    def setUp(self):
+        try:
+            import settings
+        except ImportError:
+            self.assertTrue(False)
+
+    def break_row(self):
+        """ Print a row separation for logs and stdout """
+        termprint("", "\n")
+        termprint("INFO", "-".join(["-" for x in range(0, 50)]))
+        termprint("", "\n\n")
+
+    def test_settings(self):
+        """ Test the settings exist here """
+        self.assertTrue(getattr(settings, "AMI_USER"))
+        self.assertTrue(getattr(settings, "AMI_PASS"))
+        self.assertTrue(getattr(settings, "PBX"))
+        self.break_row()
+
+
+if __name__ == '__main__':
+    suite = TestSuite()
+    suite.addTest(TestAMIBase("test_settings"))
+    TextTestRunner(verbosity=2).run(suite)
+
+
