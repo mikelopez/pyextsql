f6346507436c418d2492fb2eaa9baf13a139c8b8
Mike Lopez <dev@scidentify.info>
2013-08-02 04:14:07 -0400

    the files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='dbaccess',
+      version='0.1',
+      description='Python / MySQL - SQLAlchemy wrapper',
+      author='Marcos Lopez',
+      license='MIT',
+      py_modules=['dbaccess.dbaccess'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Terminals'
+          ]
+      )
